628348a9d79f43966b98f8e189e4b59d7384f5e2
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-03-11 11:40:59 -0300

    ensure LocalASvePurchases delete cache with given key

--- src/domain/usecases/data/usecases/local-save-purchases.spec.ts
+++ src/domain/usecases/data/usecases/local-save-purchases.spec.ts
@@ -3,23 +3,25 @@ class LocalSavePurchases{
     constructor(private readonly cacheStore:ICacheStore){}
 
     async save(): Promise<void>{
-        this.cacheStore.delete();
+        this.cacheStore.delete('purchases');
     };
 
 }
 
 interface ICacheStore{
 
-    delete() : void;
+    delete(key:string) : void;
   
 }
 
 class CacheStoreSpy implements ICacheStore{
   
     deleteCallsCount = 0;
+    key:string;
 
-    delete(): void {
+    delete(key:string): void {
         this.deleteCallsCount ++;
+        this.key = key;
     }
 }
 
@@ -37,7 +39,6 @@ describe('LocalSAvePurchases', () => {
     test('Should not delete cache on sut.init',  () => {
 
         const {cacheStore} = makeSut();
-        new LocalSavePurchases(cacheStore);
 
         expect(cacheStore.deleteCallsCount).toBe(0);
     })
@@ -45,9 +46,25 @@ describe('LocalSAvePurchases', () => {
     test('Should not delete cache on sut.save', async () => {
 
         const {sut,cacheStore} = makeSut();
-        
-        await sut.save();
 
+        await sut.save();
         expect(cacheStore.deleteCallsCount).toBe(1);
+    })
+
+    test('Should not delete cache on sut.save', async () => {
+
+        const {sut,cacheStore} = makeSut();
+
+        await sut.save();
+        expect(cacheStore.deleteCallsCount).toBe(1);
+    })
+
+
+    test('Should call delete with correct key', async () => {
+
+        const {sut,cacheStore} = makeSut();
+
+        await sut.save();
+        expect(cacheStore.key).toBe("purchases");
     })
 })
