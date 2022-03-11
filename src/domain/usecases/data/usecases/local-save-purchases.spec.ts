44d19e0a21a21779ce3aecfbc0bd06df796e91a7
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-03-11 11:22:58 -0300

    ensure LocalASvePurchases  delete old on save

--- src/domain/usecases/data/usecases/local-save-purchases.spec.ts
+++ src/domain/usecases/data/usecases/local-save-purchases.spec.ts
@@ -2,24 +2,44 @@ class LocalSavePurchases{
 
     constructor(private readonly cacheStore:ICacheStore){}
 
+    async save(): Promise<void>{
+        this.cacheStore.delete();
+    };
+
 }
 
 interface ICacheStore{
+
+    delete() : void;
   
 }
 
 class CacheStoreSpy implements ICacheStore{
   
     deleteCallsCount = 0;
+
+    delete(): void {
+        this.deleteCallsCount ++;
+    }
 }
 
 
 describe('LocalSAvePurchases', () => {
-    test('Should not delete dache on sut.init',  () => {
+    test('Should not delete cache on sut.init',  () => {
 
         const cacheStore = new CacheStoreSpy()
         new LocalSavePurchases(cacheStore);
 
         expect(cacheStore.deleteCallsCount).toBe(0);
+    })
+
+    test('Should not delete cache on sut.save', async () => {
+
+        const cacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore);
+        
+        await sut.save();
+
+        expect(cacheStore.deleteCallsCount).toBe(1);
     })
 })
